7766ec037882b0ef75e3045483472ad1d924a32e
Guillaume Turcotte <guillaume.turcotte2@ssc-spc.gc.ca>
2023-07-28 11:43:18 -0400

    added a slightly different script that should work

--- teamsPolicies/policies.ps1
+++ teamsPolicies/policies.ps1
@@ -5,12 +5,13 @@ Connect-MicrosoftTeams
 Connect-ExchangeOnline
 
 $DL = "cio-dtoemployees-dpi-btnemployes@ssc-spc.gc.ca"
-$PolicyName = "OpenAI Poc Chatbot"
+$PolicyName = "AzureOpenAI-chatbot-Pilot"
 
 $Users = Get-DistributionGroupMember -Identity $DL | Select-Object DisplayName, PrimarySmtpAddress, ExternalEmailAddress, Alias
 
 foreach($user in $Users){
     Write-Host $user.PrimarySmtpAddress #for debug
     # create policy for application
-   Grant-CsTeamsMeetingPolicy -Identity $user -PolicyName $PolicyName
+   #Grant-CsTeamsMeetingPolicy -Identity $user -PolicyName $PolicyName
+   Get-CsOnlineUser -Identity $user.PrimarySmtpAddress | grant-csteamsmeetingpolicy -policyname $PolicyName
 }
